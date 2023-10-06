f42ef60c0a7e6d2e20b43ee631a8d8b2c5085070
ach-faisal-rid <142707024+ach-faisal-rid@users.noreply.github.com>
2023-10-06 19:47:18 +0700

    book-get.php
    Update

--- book-get.php
+++ book-get.php
@@ -4,15 +4,12 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     echo json_encode(["error" => "Method tidak sesuai"]);
     exit();
 }
-// echo "metode sesuai";
-// die;
 
 require_once __DIR__ . "/koneksi.php";
 // Query untuk mengambil semua data buku tanpa pencarian
 $sql = "SELECT * FROM book ORDER BY id DESC";
 // kueri ambil semua buku dan mengurutkan id urutan menurun
 $query = mysqli_query($conn, $sql);
-// print_r($query);
 if ($query) {
     $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
