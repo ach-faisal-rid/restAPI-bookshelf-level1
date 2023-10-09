27e6ec5fe9b8a1e96faff52a34b4ab28f286034d
ach-faisal-rid <142707024+ach-faisal-rid@users.noreply.github.com>
2023-10-09 11:35:55 +0700

    Update koneksi.php
    ada dua perbedaan untuk user 👤 mysql yang pertama yang memakai password root dan yang tidak atau blank

--- koneksi.php
+++ koneksi.php
@@ -11,7 +11,8 @@ $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-// ec<?php
+// echo "connection successfully";
+<?php
 $servername = "localhost";
 $username = "root";
 $password = ""; // kalau tidak kalian password biarkan kosong
