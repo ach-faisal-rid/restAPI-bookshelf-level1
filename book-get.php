bce546ba65ab58ad72fb02ed7665173a53a59c71
ach-faisal-rid <142707024+ach-faisal-rid@users.noreply.github.com>
2023-10-09 11:33:06 +0700

    Update book-get.php
    hapus kode dibawahnya

--- book-get.php
+++ book-get.php
@@ -26,30 +26,3 @@ http_response_code(500);
 echo json_encode(array("error" => "Gagal mengambil data dari database."));
 exit();
 ?>
-<?php
-header('Content-Type: application/json');
-if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-    echo json_encode(["error" => "Method tidak sesuai"]);
-    exit();
-}
-
-require_once __DIR__ . "/koneksi.php";
-$sql = "SELECT * FROM book ORDER BY id DESC";
-$query = mysqli_query($conn, $sql);
-
-if ($query) {
-    $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
-    $response = array(
-        "data" => $result,
-    );
-    http_response_code(200);
-    echo json_encode($response);
-    exit();
-}
-
-// ketika query gagal
-http_response_code(500);
-echo json_encode(array("error" => "Gagal mengambil data dari database"));
-exit();
-
-?>
