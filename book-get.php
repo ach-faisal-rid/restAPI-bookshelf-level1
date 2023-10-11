f6bb782659a3335feeee4bc53af9dfe3a0786df4
ach-faisal-rid <142707024+ach-faisal-rid@users.noreply.github.com>
2023-10-11 08:45:57 +0700

    Update book-get.php

--- book-get.php
+++ book-get.php
@@ -1,3 +1,4 @@
+// case 1
 <?php
 header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
@@ -28,5 +29,44 @@ if ($query) {
 // Ketika query error
 http_response_code(500);
 echo json_encode(array("error" => "Gagal mengambil data dari database."));
+exit();
+?>
+
+// case 2
+<?php
+header('Content-Type: application/json');
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(403);
+    echo json_encode(["error" => "Method tidak sesuai"]);
+    exit();
+}
+require_once __DIR__ . "/koneksi.php";
+
+function getBooks() {
+    global $conn;
+    $sql = "SELECT * FROM book ORDER BY id DESC";
+    $query = mysqli_query($conn, $sql);
+
+    if ($query) {
+        $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+        return $result;
+    } else {
+        return null; // Kembalikan null jika ada kesalahan
+    }
+}
+
+$data = getBooks();
+
+if ($data !== null) {
+    $response = array(
+        "data" => $data,
+    );
+    http_response_code(200);
+    echo json_encode($response);
+} else {
+    http_response_code(500);
+    echo json_encode(array("error" => mysqli_error($conn))); // Tampilkan pesan kesalahan database yang lebih spesifik
+}
+
 exit();
 ?>
